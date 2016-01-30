7242d06db486114f29fa33ae2fd905ae0306930b
Alexander Devine <alexdevine387@gmail.com>
2016-01-30 14:44:15 -0700

    Added two images to html in centerDiv, renamed images, changed script.js so as to add veggies to centerDiv

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@ for (var i = 0; i < 5; i++) {
     var a = $('input[name=topping0]:checked').val();
 
     $('input[name=topping' + i + ']').on('change', function () {
-        var a = $(this).val();
-        console.log(a);
+        var b = $(this).attr("id");
+        var image = document.getElementById("top");
+        image.src = 'toppingImages/' + b + '.png '
+        console.log(b);
     });
 }
